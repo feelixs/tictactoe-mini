451e6bc7db4758d0d26d3a25226970085fd8bf09
Michael Felix <myellahouse@gmail.com>
2025-06-16 14:04:38 -0400

    add generated code again

---
+++ dotnet/DataClasses/LanguageToken.cs
@@ -0,0 +1,14 @@
+using SqlOnAir.DotNet.Lib.DataClasses.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlOnAir.DotNet.Lib.DataClasses
+{
+    public class LanguageToken: LanguageTokenBase
+    {
+        // Any implementation here will not be overwritten
+    }
+}
